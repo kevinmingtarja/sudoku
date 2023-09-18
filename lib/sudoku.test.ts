84458f7705792306763bf26db5cd5ce6077e3124
Kevin Mingtarja <kevin.mingtarja@u.nus.edu>
2023-09-18 22:02:44 +0800

    remove dead code

--- lib/sudoku.test.ts
+++ lib/sudoku.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from "@jest/globals"
-import { Cell, checkValidity, initializeInvalidCells } from "@/lib/sudoku"
+import { initializeInvalidCells } from "@/lib/sudoku"
 
 describe("testInitializeInvalidCells", () => {
   test("valid sudoku 0", () => {
@@ -65,50 +65,6 @@ describe("testInitializeInvalidCells", () => {
     expectedInvalidCells.add(43)
 
     const invalidCells = initializeInvalidCells(cells)
-
-    expect(invalidCells).toEqual(expectedInvalidCells)
-  })
-})
-
-describe("testCheckValidity", () => {
-  test("no invalid cells", () => {
-    const expectedInvalidCells = new Set<number>()
-
-    const input =
-      "52...6.........7.13...........4..8..6......5...........418.........3..2...87....."
-    const cells: Cell[] = new Array(81).fill(0)
-    for (let i = 0; i < 81; i++) {
-      cells[i] = {
-        value: input[i],
-        isEditable: input[i] === ".",
-        isInvalid: false, // assume all initial values are valid
-      }
-    }
-
-    const invalidCells = checkValidity(0, cells)
-
-    expect(invalidCells).toEqual(expectedInvalidCells)
-  })
-
-  test("invalid row, col, and subgrid", () => {
-    const expectedInvalidCells = new Set<number>()
-
-    const input =
-      "52...5.........7.13...........5..5..6......5...........418.........3..2...87....."
-    const cells: Cell[] = new Array(81).fill(0)
-    for (let i = 0; i < 81; i++) {
-      cells[i] = {
-        value: input[i],
-        isEditable: input[i] === ".",
-        isInvalid: false, // assume all initial values are valid
-      }
-    }
-
-    // cell 30, 33, 43 are also all 5 (invalid), but it's not captured because it's not reachable from the starting point
-    expectedInvalidCells.add(0)
-    expectedInvalidCells.add(5)
-
-    const invalidCells = checkValidity(0, cells)
 
     expect(invalidCells).toEqual(expectedInvalidCells)
   })
